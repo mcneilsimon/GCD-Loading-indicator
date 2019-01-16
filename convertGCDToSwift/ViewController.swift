0f519481b850b73376e972694385afc8a81dc624
Simon Mc Neil <mcneils@sheridancollege.ca>
2019-01-16 12:18:44 -0500

    pushing project to new repository

--- convertGCDToSwift/ViewController.swift
+++ convertGCDToSwift/ViewController.swift
@@ -9,17 +9,72 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var result: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var downloadCompleted: UILabel!
+    @IBOutlet weak var downloadButton: UIButton!
+    
+    var refToQueueOperation: DispatchQueue?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    //thread execution starts
+    @IBAction func actionButton(_ sender: UIButton) {
+        refToQueueOperation = DispatchQueue(label: "queue")
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        refToQueueOperation?.async(execute: {() -> Void in
+            self.longRunningOperation()
+        })
+        
     }
+    //loops the progress bar until completed
+    func longRunningOperation(){
+        for i in 0..<101 {
+            self.performUpdate(i) {
+                Thread.sleep(forTimeInterval: 0.1)
+            }
+        }
+    }
+    
+    //thread execution begings
+    func performUpdate(_ index: Int, selector: @escaping () -> ()) {
 
-
+        DispatchQueue.main.async(execute: {() -> Void in
+            
+            self.downloadCompleted.text = ""
+            
+            //once index value is greater then 10, then the download will be completed
+            if (index >= 100) {
+                self.downloadButton.isEnabled = true
+                self.downloadButton.alpha = 1.0
+                self.result.text = "100%"
+                self.downloadCompleted.text = "Download Completed!"
+                self.progressBar.progress=0
+            }
+            else{
+                self.downloadButton.isEnabled = false
+                self.downloadButton.alpha = 0.25
+                let progressResults = Float(index) / 100.0
+                let loadingActivity = index != 0
+                self.progressBar.setProgress(progressResults, animated: loadingActivity)
+                self.result.text = ("\(index)%")
+            }
+        })
+        selector()
+    }
 }
+
+
+
+
+
+
+
+
+
+
 
